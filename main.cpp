9fd3384ee71e3e87012054959c99452d7b418b0f
maple <hinokuchi.r@gmail.com>
2022-09-25 00:21:20 -0400

    changed to glad2 w cmake subdirectory

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <glad/glad.h>
+#include <glad/gl.h>
 //#include <GL/gl.h>
 #include <GLFW/glfw3.h>
 
@@ -29,14 +29,28 @@ int main (int argc, char * argv[]){
         return -1;
     }
     glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        std::cout << "failed to initialize GLAD" << std::endl;
+    }
+    /*
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cout << "failed to initialize GLAD" << std::endl;
         return -1;
     }
+    */
     glViewport(0, 0, 800, 600);
     //glViewport();
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+        
+    float triVerts[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+    unsigned int VBO;
+    glGenBuffers(1,&VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triVerts), triVerts, GL_STATIC_DRAW);
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
         //rendering
@@ -44,7 +58,7 @@ int main (int argc, char * argv[]){
         glad_glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         //
-
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
