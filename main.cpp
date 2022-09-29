1032341fa7a258a86baafe4d8d0abd35f5cec277
maple <hinokuchi.r@gmail.com>
2022-09-29 15:14:53 -0400

    index buffers

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
- #include <glad/gl.h>
-//#include <GL/gl.h>
+#include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
 #include <cstddef>
 #include <iostream>
 #include <ostream>
+
+#include <vector>
 
 const char *vertexshadersource = "\n"
     "#version 330 core\n"
@@ -37,6 +38,8 @@ int testNumber(int input){
 }
 
 int main (int argc, char * argv[]){
+    //int myInt = 0;
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
@@ -63,12 +66,27 @@ int main (int argc, char * argv[]){
     glViewport(0, 0, 800, 600);
     //glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    /*
     float triVerts[] = {
         -0.5f, -0.5f, 0.0f,
         0.5f, -0.5f, 0.0f,
         0.0f, 0.5f, 0.0f
     };
-    
+    */
+
+    float indexVerts[] = {
+        //
+        0.5f, 0.5f, 0.0f,       //top right
+        0.5f, -0.5f, 0.0f,      //bottom right
+        -0.5f, -0.5f, 0.0f,     //bottom left
+        -0.5f, 0.5f, 0.0f       //bottom right
+    };
+
+    unsigned int indicies[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
     //Setting vertex shader
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -100,17 +118,26 @@ int main (int argc, char * argv[]){
     
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-
-    //Seting vertex buffer object
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(triVerts), triVerts, GL_STATIC_DRAW);
-
+    
     //setting vertex ARRAY object
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
+
+    //setting element buffer object
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+
+    //Seting vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(triVerts), triVerts, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(indexVerts), indexVerts, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
 
     //vertex attribute
     //location in vertShader, size of vertex attribute (vec3 so it's 3), using floats, not normalized, sizeof stride (3 floats, so 3 times the size of float), offset is 0 cast as void.
@@ -128,14 +155,15 @@ int main (int argc, char * argv[]){
         //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glad_glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        //
+        
         
         //draw triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
-
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+        
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
